6d1f0ea5ccaec4ae47a00f22e865c1bb57b044ba
tsiviv <t0527698098@gmail.com>
2025-06-01 16:22:33 +0300

    F

--- run.bat
+++ run.bat
@@ -1,3 +1,17 @@
 @echo off
+
+REM בדיקה אם node מותקן
+node -v >nul 2>&1
+IF %ERRORLEVEL% NEQ 0 (
+    echo Node.js לא מותקן. מבצע התקנה...
+    start /wait msiexec /i "%~dp0node-install.msi" /quiet
+    echo ממתין לסיום ההתקנה...
+    timeout /t 5
+) ELSE (
+    echo Node.js כבר מותקן. ממשיך להריץ את הפרויקט...
+)
+
+REM הרצת הפרויקט
 npm start
+
 pause
